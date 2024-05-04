428625035f3e455fd1a4c829d847cf64df159687
Muhammad Rayhan Gibran <mraygbrn@gmail.com>
2024-05-04 17:29:26 +0700

    test

--- public/Js/preloader.js
+++
@@ -1,7 +0,0 @@
-const form = document.querySelectorAll("form");
-form.forEach(e => {
-  e.addEventListener('submit', function () {
-    document.querySelector("html").style.cursor = "wait";
-    document.querySelector(".loading-wrapper").classList.remove('d-none');
-  })
-});
