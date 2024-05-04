3fecd02bb15af1101ebd740cefb9e5ee9db48be0
Muhammad Rayhan Gibran <mraygbrn@gmail.com>
2024-05-04 16:48:38 +0700

    fix(loader): fix loader trigger

--- public/Js/preloader.js
+++ public/Js/preloader.js
@@ -1,6 +1,6 @@
-const submitBtns = document.querySelectorAll(".btn-submit");
-submitBtns.forEach(e => {
-  e.addEventListener('click', function () {
+const form = document.querySelectorAll("form");
+form.forEach(e => {
+  e.addEventListener('submit', function () {
     document.querySelector("html").style.cursor = "wait";
     document.querySelector(".loading-wrapper").classList.remove('d-none');
   })
