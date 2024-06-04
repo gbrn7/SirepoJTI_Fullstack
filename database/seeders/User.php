8929c72ed808ed5bd444fab746507fe3f3c720bf
Muhammad Rayhan Gibran <mraygbrn@gmail.com>
2024-06-04 13:24:12 +0700

    fix(seed): fix seeder

--- database/seeders/User.php
+++ database/seeders/User.php
@@ -14,35 +14,35 @@ class User extends Seeder
      */
     public function run(): void
     {
-        $faker = Faker::create('id_ID'); //the argument is used for country code
+        // $faker = Faker::create('id_ID'); //the argument is used for country code
 
-        $user = ModelsUser::create(
-            [
-            'id_program_study' => 1,
-            'name' => 'Farhan',
-            'username' => 'farhan12',
-            'email' => 'farhan123@gmail.com',
-            'password' => Hash::make('userpass'),
-            'created_at' => now(),
-            'updated_at' => now()
-            ]
-        );
-        $user->assignRole('user');
+        // $user = ModelsUser::create(
+        //     [
+        //         'id_program_study' => 1,
+        //         'name' => 'Farhan',
+        //         'username' => 'farhan12',
+        //         'email' => 'farhan123@gmail.com',
+        //         'password' => Hash::make('userpass'),
+        //         'created_at' => now(),
+        //         'updated_at' => now()
+        //     ]
+        // );
+        // $user->assignRole('user');
 
 
-        for ($i=0; $i < 100; $i++) { 
-            $user = ModelsUser::create([
-                'id_program_study' => $faker->numberBetween(1,2),
-                'name' => $faker->name(),
-                'username' => $faker->userName(),
-                'email' => $faker->email(),
-                'password' => Hash::make('userpass'),
-                'created_at' => now(),
-                'updated_at' => now()
-            ]);
+        // for ($i=0; $i < 100; $i++) { 
+        //     $user = ModelsUser::create([
+        //         'id_program_study' => $faker->numberBetween(1,2),
+        //         'name' => $faker->name(),
+        //         'username' => $faker->userName(),
+        //         'email' => $faker->email(),
+        //         'password' => Hash::make('userpass'),
+        //         'created_at' => now(),
+        //         'updated_at' => now()
+        //     ]);
 
-            $user->assignRole('user');
-        }
+        //     $user->assignRole('user');
+        // }
 
     }
 }
