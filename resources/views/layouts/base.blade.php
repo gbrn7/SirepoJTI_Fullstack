12f39a4f294429b176b992972ddee292568af490
Muhammad Rayhan Gibran <mraygbrn@gmail.com>
2025-04-11 11:06:56 +0700

    feat(index): add prevent script from search engine index

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -4,6 +4,9 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="robots" content="noindex">
+  <meta name="googlebot" content="noindex">
+
   <title>Sirepo-JTI | @yield('title')</title>
   <!-- Icon -->
   <link rel="icon" type="image/x-icon" href="{{asset('img/jti_logo.png')}}" />
