15c7c6f8f0787191deca30ee8cb313e279b081ab
Muhammad Rayhan Gibran <mraygbrn@gmail.com>
2025-06-05 00:31:38 +0700

    fix

--- src/resources/views/admin_views/student/index.blade.php
+++ src/resources/views/admin_views/student/index.blade.php
@@ -183,7 +183,7 @@
   <div class="modal-dialog ">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="myModalLabel">Delete User</h5>
+        <h5 class="modal-title" id="myModalLabel">Hapus Mahasiswa</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
@@ -195,9 +195,9 @@
         @method('delete')
         @csrf
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
           <button type="submit" id="deletecriteria" data-cy="btn-delete-confirm"
-            class="btn btn-danger btn-submit">Delete</button>
+            class="btn btn-danger btn-submit">Hapus</button>
       </form>
     </div>
   </div>
