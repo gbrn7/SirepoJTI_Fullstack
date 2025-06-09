68d52f528096fe6063c12d531cbed22a64c7bf97
Muhammad Rayhan Gibran <mraygbrn@gmail.com>
2025-06-09 23:07:09 +0700

    test

--- .build.sh
+++ .build.sh
@@ -2,19 +2,16 @@
 
 echo "Running deploy script"
 
-echo "[1/] Pulling from GitHub"
+echo "[1/5] Pulling from GitHub"
 git pull origin
 
-echo "[2/] Creating database if one isn't found"
-touch database/database.sqlite
-
-echo "[3/] Installing packages using composer"
+echo "[2/4] Installing packages using composer"
 composer install
 
-echo "[4/] Migrating database"
+echo "[3/4] Migrating database"
 php artisan migrate --force
 
-echo "[5/] Cache config, routes, dan views"
+echo "[4/4] Cache config, routes, dan views"
 php artisan optimize
 
 echo "The app has been built and deployed!"
