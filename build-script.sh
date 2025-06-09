c1281fecd0e4d4821d10e7b9e77f771f581eb42f
Muhammad Rayhan Gibran <mraygbrn@gmail.com>
2025-06-09 23:18:17 +0700

    test

--- build-script.sh
+++ build-script.sh
@@ -3,13 +3,16 @@ echo "Running deploy script"
 echo "[1/5] Pulling from GitHub"
 git pull origin
 
-echo "[2/4] Installing packages using composer"
+echo "[2/5] Installing packages using composer"
 composer install
 
-echo "[3/4] Migrating database"
+echo "[3/5] Migrating database"
 php artisan migrate --force
 
-echo "[4/4] Cache config, routes, dan views"
+echo "[4/5] Storage link"
+php artisan storage:link
+
+echo "[4/5] Cache config, routes, dan views"
 php artisan optimize
 
 echo "The app has been built and deployed!"
