c79cfebe42442268fdddf52598c8508e7d67d7db
Muhammad Rayhan Gibran <mraygbrn@gmail.com>
2025-06-10 12:35:13 +0700

    test

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "https://sirepo-jti.raygbrn.site",
+    baseUrl: "http://127.0.0.1:8000",
     viewportHeight: 720,
     viewportWidth: 1024,
     video: false,
